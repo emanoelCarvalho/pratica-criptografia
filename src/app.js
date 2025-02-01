aee015af841b43d81a7287a3510becb966f19a6b
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-01-31 21:40:30 -0300

    feat: enable CORS support in the application

--- src/app.js
+++ src/app.js
@@ -6,6 +6,7 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
 app.use(express.static("public"));
 
 app.post("/api/criptografar", criptografiaController.criptografarFrase);
