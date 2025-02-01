2c200219af9ef07dc5a35cbd11c340dffebba69c
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-01-31 21:41:36 -0300

    feat: import CORS middleware in app.js for cross-origin support

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const criptografiaController = require("./controllers/criptografiaController");
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
