8869874b52f8dea33bc4b9c7fa0195121cd59544
emanoelCarvalho <ehcs@discente.ifpe.edu.br>
2025-02-01 21:39:39 -0300

    feat: refactor form submission in index.js to use async/await for improved readability

--- client/index.js
+++ client/index.js
@@ -1,22 +1,19 @@
 document
   .getElementById("formCriptografar")
-  .addEventListener("submit", function (event) {
+  .addEventListener("submit", async function (event) {
     event.preventDefault();
+
     const frase = document.getElementById("frase").value;
 
-    fetch("/api/criptografar", {
+    const response = await fetch("http://localhost:3000/criptografar", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
+
       body: JSON.stringify({ frase }),
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        document.getElementById("resultado").innerText =
-          data.fraseCriptografada;
-      })
-      .catch((error) => {
-        console.error("Erro:", error);
-      });
+    });
   });
+
+const data = await response.json();
+document.getElementById("resultado").innerText = data.frase;
